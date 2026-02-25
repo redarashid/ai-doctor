5798b5136d7861244e01331b8c8b60b66cba74ae
redarashid <redarashid824@gmail.com>
2026-02-25 13:06:35 +0200

    first commit

---
+++ app/login/page.jsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    console.log({ email, password });
+
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <form
+        onSubmit={handleLogin}
+        className="bg-white p-6 rounded-xl shadow-md w-96 space-y-4"
+      >
+        <h2 className="text-2xl font-bold text-center">Login</h2>
+
+        <input
+          type="email"
+          placeholder="Email"
+          className="w-full border p-2 rounded"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          className="w-full border p-2 rounded"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button className="w-full bg-blue-600 text-white p-2 rounded hover:bg-blue-700">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
